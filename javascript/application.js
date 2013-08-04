341c905dff2875dc0ea97cae6fe9a2d01631ff86
stefanRitter <stefan@stefanritter.com>
2013-08-04 20:25:42 +0100

    init

---
+++ javascript/application.js
@@ -0,0 +1,12 @@
+/*
+ *
+ *  Hi there, have a look at my source files here:
+ *  https://github.com/stefanRitter/
+ *
+ */
+
+(function() {
+  jQuery(function($) {
+    // ready
+  });
+}).call(this);
