1a38f5e35bf1716ae29766fc9260a330df985f84
stefanRitter <stefan@stefanritter.com>
2013-08-04 21:13:09 +0100

    add hammer.js

--- javascript/application.js
+++ javascript/application.js
@@ -7,6 +7,22 @@
 
 (function() {
   jQuery(function($) {
-    // ready
+    
+    var events = [
+                  "tap",
+                  "doubletap",
+                  "dragup", "dragdown", "dragleft", "dragright",
+                  "swipe", "swipeup", "swipedown", "swipeleft", "swiperight",
+                  "rotate",
+                  "pinchin", "pinchout"
+                 ],
+        canvas = $('canvas');
+
+    for (var i = 0; i < events.length; i++) {
+      canvas.hammer().on( events[i] , function(e) {
+        e.preventDefault(); e.stopPropagation();
+        $('.events').text(events[i]);
+      });
+    }
   });
 }).call(this);
