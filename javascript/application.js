44c5ba2580ac0607803be4be4b3e1fa77763d378
stefanRitter <stefan@stefanritter.com>
2013-08-04 21:35:29 +0100

    track basic events

--- javascript/application.js
+++ javascript/application.js
@@ -4,25 +4,46 @@
  *  https://github.com/stefanRitter/
  *
  */
-
+var gEvents = [ "tap",
+                "doubletap",
+                "dragup", "dragdown", "dragleft", "dragright",
+                "swipe", "swipeup", "swipedown", "swipeleft", "swiperight",
+                "rotate",
+                "pinchin", "pinchout"
+              ];
 (function() {
   jQuery(function($) {
     
-    var events = [
-                  "tap",
-                  "doubletap",
-                  "dragup", "dragdown", "dragleft", "dragright",
-                  "swipe", "swipeup", "swipedown", "swipeleft", "swiperight",
-                  "rotate",
-                  "pinchin", "pinchout"
-                 ],
-        canvas = $('canvas');
+    var canvas = $('canvas');
 
-    for (var i = 0; i < events.length; i++) {
-      canvas.hammer().on( events[i] , function(e) {
-        e.preventDefault(); e.stopPropagation();
-        $('.events').text(events[i]);
-      });
-    }
+    canvas.hammer().on('tap' , function(e) {
+      e.preventDefault(); e.stopPropagation();
+      $('.events').prepend('<p>tap</p>');
+    });
+
+    canvas.hammer().on('doubletap' , function(e) {
+      e.preventDefault(); e.stopPropagation();
+      $('.events').prepend('<p>doubletap</p>');
+    });
+
+    canvas.on('dragup' , function(e) {
+      e.preventDefault(); e.stopPropagation();
+      $('.events').prepend('<p>dragup</p>');
+    });
+
+    canvas.hammer().on('swipe' , function(e) {
+      e.preventDefault(); e.stopPropagation();
+      $('.events').prepend('<p>swipe</p>');
+    });
+
+    canvas.hammer().on('pinchin' , function(e) {
+      e.preventDefault(); e.stopPropagation();
+      $('.events').prepend('<p>pinchin</p>');
+    });
+
+    canvas.on('rotate' , function(e) {
+      e.preventDefault(); e.stopPropagation();
+      $('.events').prepend('<p>rotate</p>');
+    });
   });
 }).call(this);
