5bd1497be48ef8c4c6ec0fd9217ff5a1212edcf1
stefanRitter <stefan@stefanritter.com>
2013-08-10 13:46:43 +0100

    add basic drawing functions

--- javascript/application.js
+++ javascript/application.js
@@ -4,28 +4,77 @@
  *  https://github.com/stefanRitter/
  *
  */
-var gEvents = [ "tap",
+
+
+// global Hammer.js touch events
+var gEvents = [ "tap", "hold",
                 "doubletap",
                 "dragup", "dragdown", "dragleft", "dragright",
                 "swipe", "swipeup", "swipedown", "swipeleft", "swiperight",
                 "rotate",
                 "pinchin", "pinchout"
               ];
+
+
 (function() {
   jQuery(function($) {
     
-    var canvas = $('canvas');
+    var canvas = document.getElementsByTagName('canvas')[0],
+        $canvas = $('canvas'),
+        context = canvas.getContext('2d'),
+        objects = [];
+        
+    function Circle() {
+      this.x = 0;
+      this.y = 0;
+      this.color = 0.0;
+      this.life = 100;
+      this.render = function() {
+        if (this.life >= 0) {
+          if( this.life <= 50) {
+            context.fillStyle = this.color + this.life/50.0 + ")";
+          } else {
+            context.fillStyle = this.color + 1 + ")";
+          }
+          context.beginPath();
+          context.arc(this.x, this.y, 25, 0, Math.PI*2, true);
+          context.closePath();
+          context.fill();
+          this.life--;
+        }
+      };
+    }
+    
 
-    canvas.hammer().on('tap' , function(e) {
+    function randomColor() {
+      var r = Math.floor((Math.random()*255)+1),
+          g = Math.floor((Math.random()*100)+1),
+          b = Math.floor((Math.random()*100)+1);
+      return "rgba(" + r + "," + g + "," + b + ",";
+    }
+
+
+    // setup canvas
+    canvas.width = document.width;
+    canvas.height = document.height;
+    
+
+    // touch event handlers
+    $canvas.hammer().on('drag' , function(e) {
       e.preventDefault(); e.stopPropagation();
       $('.events').prepend('<p>tap</p>');
+      var object = new Circle();
+      object.x = e.gesture.center.pageX-25;
+      object.y = e.gesture.center.pageY-25;
+      object.color = randomColor();
+      objects.push(object);
     });
 
-    canvas.hammer().on('doubletap' , function(e) {
+    $canvas.hammer().on('doubletap' , function(e) {
       e.preventDefault(); e.stopPropagation();
       $('.events').prepend('<p>doubletap</p>');
     });
-
+/*
     canvas.on('dragup' , function(e) {
       e.preventDefault(); e.stopPropagation();
       $('.events').prepend('<p>dragup</p>');
@@ -45,5 +94,17 @@ var gEvents = [ "tap",
       e.preventDefault(); e.stopPropagation();
       $('.events').prepend('<p>rotate</p>');
     });
+*/
+
+
+
+    function render() {
+      context.clearRect(0,0,canvas.width,canvas.height);
+      for (var i = 0; i < objects.length; i++) {
+        objects[i].render();
+      }
+      window.requestAnimationFrame(render);
+    }
+    window.requestAnimationFrame(render);
   });
 }).call(this);
